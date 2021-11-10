b877fc03b301923a3d225ae172e36306116ec00f
elineri <elin.ericstam@gmail.com>
2021-11-10 15:23:16 +0100

    Skapat en foreach-loop som skriver ut alla objekt i Stacken samt hur många items som är kvar.

--- CollectionGenerics/Program.cs
+++ CollectionGenerics/Program.cs
@@ -9,10 +9,10 @@ namespace CollectionGenerics
         {
 
             Employee E1 = new Employee() { ID = 101, Name = "Elin", Gender = "Kvinna", Salary = 25000 };
-            Employee E2 = new Employee() { ID = 101, Name = "Anas", Gender = "Man", Salary = 30000 };
-            Employee E3 = new Employee() { ID = 101, Name = "Tobias", Gender = "Man", Salary = 28000 };
-            Employee E4 = new Employee() { ID = 101, Name = "Malin", Gender = "Kvinna", Salary = 22000 };
-            Employee E5 = new Employee() { ID = 101, Name = "Oskar", Gender = "Man", Salary = 24000 };
+            Employee E2 = new Employee() { ID = 102, Name = "Anas", Gender = "Man", Salary = 30000 };
+            Employee E3 = new Employee() { ID = 103, Name = "Tobias", Gender = "Man", Salary = 28000 };
+            Employee E4 = new Employee() { ID = 104, Name = "Malin", Gender = "Kvinna", Salary = 22000 };
+            Employee E5 = new Employee() { ID = 105, Name = "Oskar", Gender = "Man", Salary = 24000 };
 
             Stack<Employee> EmpSta = new Stack<Employee>();
 
@@ -21,6 +21,12 @@ namespace CollectionGenerics
             EmpSta.Push(E3);
             EmpSta.Push(E4);
             EmpSta.Push(E5);
+
+            foreach (Employee item in EmpSta)
+            {
+                Console.WriteLine($"ID: {item.ID}\nName: {item.Name}\nGender: {item.Gender}\nSalary: {item.Salary}");
+                Console.WriteLine($"Items in the Stack: {EmpSta.Count}\n");
+            }
         }
     }
 }
