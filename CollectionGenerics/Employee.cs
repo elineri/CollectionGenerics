bdc04d45c93c814c33d1ec947e9de20d46c5a8f5
elineri <elin.ericstam@gmail.com>
2021-11-10 15:04:38 +0100

    Skapat klass Employee och en Stack.

---
+++ CollectionGenerics/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionGenerics
+{
+    class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Salary { get; set; }
+    }
+}
